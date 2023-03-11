6c8d62c9ad828b93e17599b566fbc2a1ae1af7cd
Mari test <missuri82@gmail.com>
2023-03-11 22:17:35 +0300

    modified added

--- BmiService.java
+++
@@ -1,7 +0,0 @@
-public class BmiService {
-    public double calculate (double massa, double height){
-        double h = height * height;
-        double index = massa / h;
-        return (int) index;
-    }
-}
